58e140ba13adb52970646db76edee2b3ebf29fdc
codedghost2 <codedghost2@gmail.com>
2020-03-31 17:02:58 +0100

    Request a practice song

---
+++ CoreCodedChatbot.Web/CoreCodedChatbot.Web/ViewModels/Development/RequestPracticeSongViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreCodedChatbot.Web.ViewModels.Development
+{
+    public class RequestPracticeSongViewModel
+    {
+        [StringLength(100, MinimumLength = 10)]
+        [Display(Name = "Please enter the song name you wish Coded to learn *")]
+        [Required(ErrorMessage = "This field is required")]
+        public string SongName { get; set; }
+
+        [Display(Name = "Any extra info for learning this song?")]
+        public string ExtraInformation { get; set; }
+    }
+}
