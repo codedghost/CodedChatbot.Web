ca4c83ae097ca4a43997a6e5b1615fd4d6624c7b
codedghost2 <codedghost2@gmail.com>
2020-09-07 21:53:02 +0100

    Fixing Twitch Stream preview panel

--- CoreCodedChatbot.Web/CoreCodedChatbot.Web/Controllers/HomeController.cs
+++ CoreCodedChatbot.Web/CoreCodedChatbot.Web/Controllers/HomeController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreCodedChatbot.Config;
+using CoreCodedChatbot.Web.ViewModels.Home;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreCodedChatbot.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfigService _configService;
+
+        public HomeController(IConfigService configService)
+        {
+            _configService = configService;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var viewModel = new HomepageViewModel
+            {
+                PreviewTwitchChannelName = _configService.Get<string>("StreamerChannel"),
+                Domain = HttpContext.Request.Host.Host
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ CoreCodedChatbot.Web/CoreCodedChatbot.Web/ViewModels/Home/HomepageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreCodedChatbot.Web.ViewModels.Home
+{
+    public class HomepageViewModel
+    {
+        public string PreviewTwitchChannelName { get; set; }
+        public string Domain { get; set; }
+    }
+}
