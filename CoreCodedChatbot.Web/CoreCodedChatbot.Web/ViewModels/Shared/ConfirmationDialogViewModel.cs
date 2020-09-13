94ebd4b66fff02c1cc7b40e278f86e87806515c0
codedghost2 <codedghost2@gmail.com>
2020-09-13 12:23:47 +0100

    Creating generic confirmation dialog and using it on transfer user moderation area

---
+++ CoreCodedChatbot.Web/CoreCodedChatbot.Web/ViewModels/Shared/ConfirmationDialogViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CoreCodedChatbot.Web.ViewModels.Shared
+{
+    public class ConfirmationDialogViewModel
+    {
+        public string ModalId { get; set; }
+        public string ModalTitle { get; set; }
+        public string CloseButtonText { get; set; }
+        public string ConfirmationButtonText { get; set; }
+        public string BodyText { get; set; }
+        public string CloseFunctionName { get; set; }
+        public string CloseButtonAction { get; set; }
+        public string ConfirmFunctionName { get; set; }
+        public string ConfirmationButtonAction { get; set; }
+    }
+}
