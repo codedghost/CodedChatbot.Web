47c574e92f9dfeb90b74c3cf0404b34434faf96b
codedghost2 <codedghost2@gmail.com>
2021-10-03 08:59:31 +0100

    Add request function added for new UI

--- CoreCodedChatbot.Web/CoreCodedChatbot.Web/Controllers/PlaylistController.cs
+++ CoreCodedChatbot.Web/CoreCodedChatbot.Web/Controllers/PlaylistController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
 using CoreCodedChatbot.ApiContract.Enums.Playlist;
+using CoreCodedChatbot.ApiContract.RequestModels.Playlist;
 using CoreCodedChatbot.ApiContract.RequestModels.Vip;
 using CoreCodedChatbot.ApiContract.ResponseModels.Playlist.ChildModels;
 using CoreCodedChatbot.Web.Extensions;
@@ -96,5 +97,46 @@ namespace CoreCodedChatbot.Web.Controllers
 
             return Json(userPlaylistInfo);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRequest([FromBody] UiSongRequestModel request)
+        {
+            if (!User.Identities.Any(id => id.IsAuthenticated))
+                return BadRequest();
+
+            var response = await _playlistApiClient.AddWebRequest(new AddWebSongRequest
+            {
+                Title = request.songName,
+                Artist = request.artistName,
+                IsVip = request.useVipToken,
+                IsSuperVip = request.useSuperVipToken,
+                SelectedInstrument = request.instrument,
+                Username = User.Identity.Name
+            }).ConfigureAwait(false);
+
+            var errorMessage = response.Result switch
+            {
+                AddRequestResult.MaximumRegularRequests =>
+                "You already have the maximum number of regular requests in the queue! Please wait until your song has been played, edit your song, or use a VIP token",
+                AddRequestResult.NoRequestEntered => "Please enter more information",
+                AddRequestResult.NotEnoughVips => "You do not have enough VIPs to request this song at the moment",
+                AddRequestResult.OnlyOneSuper => "There can only be 1 Super VIP request in the queue at one time. Please wait a moment for this song to be played",
+                AddRequestResult.PlaylistClosed => "The playlist is currently only accepting VIP requests",
+                AddRequestResult.PlaylistVeryClosed => "The playlist is currently completely closed",
+                AddRequestResult.UnSuccessful => "An error has occurred, please wait a moment and try again",
+                AddRequestResult.Success => "Success",
+                _ => string.Empty
+            };
+            return Ok(errorMessage);
+        }
+    }
+
+    public class UiSongRequestModel
+    {
+        public string songName { get; set; }
+        public string artistName { get; set; }
+        public string instrument { get; set; }
+        public bool useVipToken { get; set; }
+        public bool useSuperVipToken { get; set; }
     }
 }
