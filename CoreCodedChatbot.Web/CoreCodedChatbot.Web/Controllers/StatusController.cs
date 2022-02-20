a62c5b137dd974e81f71fc3bf15e71a30f12dc98
CodedGhost <codedghost2@gmail.com>
2022-02-20 17:14:19 +0000

    Adding Streamer status endpoint

--- CoreCodedChatbot.Web/CoreCodedChatbot.Web/Controllers/StatusController.cs
+++ CoreCodedChatbot.Web/CoreCodedChatbot.Web/Controllers/StatusController.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreCodedChatbot.ApiClient.Interfaces.ApiClients;
+using CoreCodedChatbot.ApiContract.RequestModels.StreamStatus;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreCodedChatbot.Web.Controllers
 {
     public class StatusController : Controller
     {
+        private readonly IStreamStatusApiClient _streamStatusApiClient;
+
+        public StatusController(IStreamStatusApiClient streamStatusApiClient)
+        {
+            _streamStatusApiClient = streamStatusApiClient;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return new JsonResult(true);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Streamer(string broadcaster)
+        {
+            var status = await _streamStatusApiClient.GetStreamStatus(new GetStreamStatusRequest
+            {
+                BroadcasterUsername = broadcaster
+            });
+
+            return Ok(status);
         }
     }
 }
